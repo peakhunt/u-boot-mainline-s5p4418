80a7055ea680181bf351b6f56ff1e1afe95ca83f
Hawk Kim <hawk_kim74@hotmail.com>
2017-11-02 10:27:36 +0900

    slowly building up spl boot

--- arch/arm/mach-s5p4418/spl_boot.c
+++ arch/arm/mach-s5p4418/spl_boot.c
@@ -4,182 +4,113 @@
 #include <spl.h>
 #include <debug_uart.h>
 
+#include <asm/arch/s5p4418.h>
+#include <asm/arch/s5p4418_syscon.h>
+#include <asm/arch/s5p4418_gpio.h>
+
+static inline void
+update_pll(s5p4418_syscon_t* reg, u8 pll_num)
+{
+  writel(S5P4418_UPDATE_PLL(pll_num), &reg->clkmodereg0);
+
+  while(readl(&reg->clkmodereg0) & S5P4418_WAIT_UPDATE_PLL)
+    ;
+}
+
+static void
+setup_pll_clock(void)
+{
+  //////////////////////////////////////////////////////////////////////////
+  //
+  // PLL0 : 1 GHz for f/h/b/p clk
+  // PLL1 : 800 Mhz for DDR3
+  // PLL2 :
+  // PLL3 :
+  //
+  // fclk : 1 Ghz
+  // hclk : 250 Mhz
+  // bclk : 333 Mhz
+  // pclk : 166
+  //
+  //////////////////////////////////////////////////////////////////////////
+  s5p4418_syscon_t*   reg;
+
+
+  reg = (s5p4418_syscon_t*)S5P4418_BASE_SYS_CON;
+
+  // PLL0, SDIV: 1, MDIV: 500, PDIV : 6
+  writel(S5P4418_PLL_SDIV(1)    |
+         S5P4418_PLL_MDIV(500)  |
+         S5P4418_PLL_PDIV(6)    |
+         S5P4418_PLL_OUTDIV(0)  |
+         S5P4418_PLL_BYPASS(1)  |
+         S5P4418_PLL_PD(0)      |
+         S5P4418_PLL_SSCG_EN(0),
+         &reg->pllsetreg0);
+
+  update_pll(reg, 0);
+
+  // setup f/h clk
+  writel(S5P4418_CLKSEL_FCLKCPU0(0)   |
+         S5P4418_CLKDIV_FCLKCPU0(0)   |
+         S5P4418_CLKDIV_HCLKCPU0(3),
+         &reg->clkdivreg0);
+
+  // setup b/p clk
+  writel(S5P4418_CLKSEL_BCLK(0)       |
+         S5P4418_CLKDIV_BCLK(2)       |
+         S5P4418_CLKDIV_PCLK(1),
+         &reg->clkdivreg1);
+
+  // PLL1 SDIV : 1, MDIV: 400, PDIV: 6
+  writel(S5P4418_PLL_SDIV(1)    |
+         S5P4418_PLL_MDIV(400)  |
+         S5P4418_PLL_PDIV(6)    |
+         S5P4418_PLL_OUTDIV(0)  |
+         S5P4418_PLL_BYPASS(1)  |
+         S5P4418_PLL_PD(0)      |
+         S5P4418_PLL_SSCG_EN(0),
+         &reg->pllsetreg1);
+
+  update_pll(reg, 1);
+
+  // PLL2
+  // FIXME
+  //
+
+  //
+  // PLL3
+  // FIXME
+}
+
 void
 board_init_f(ulong dummy)
 {
-  // change pll0 to 400 Mhz
-  // p 6
-  // m 400
-  // s 2
-  //
-  // accordingly
-  //
-  // fclk : 400 / 2       = 200
-  // hclk : fclk / 2      = 100
-  // bclk : 400 / 2       = 200
-  // pclk: bclk / 2       = 100
-  //
-  writel(   (2 << 0)    |       // sdiv
-            (400 << 8)  |       // mdiv
-            (6 << 18)   |       // pdiv
-            (0 << 24)   |       // outdiv
-            (1 << 28)           // normal output
-            , 0xC0010008);
-
-  // update pll0
-  writel(   (1 << 0), 0xC0010000);
-
-  while(readl(0xC0010000) & 0x80000000)
-  {
-  }
-
-#if 0
-  //
-  // enable writing to alive register
-  // set vddctrlsetreg
-  //
-  writel(0x01,  0xc0010800);
-  writel(0x3fc, 0xc001089c);
+  setup_pll_clock();
 
   //
-  //  rtc or wdt or whatever
+  // perform entire IP peripheral reset
   //
-  writel(0x01,       0xc0010800);
-  writel(0xffffffff, 0xc0010c1c);
+  s5p4418_ip_reset(0, 0xffffffff);
+  s5p4418_ip_reset(1, 0xffffffff);
+  s5p4418_ip_reset(2, 0xffffffff);
 
-  //
-  // unknown
-  //
-  //(void)readl(0xc001027c);
-#endif
-
-  // GPIO B12 is a LED pin
-  // set it to output mode
-  // drive it to high to turn off the LED
-  //
-  // ball W24 should be set to ALTF2 for GPIO use
-  //
-  writel((0x02 << 24), 0xc001b020);        // alt func setup
-  writel((0x01 << 12), 0xc001b004);        // output enable
-  writel((0x01 << 12), 0xc001b000);        // set high
-
-  //
-  // UART0 pin  mux
-  // AD19   : TX, GPIOD18, AF1
-  // AE19   : RX, GPIOD14, AF1
-  writel((0x1 << 4),  0xc001d024);
-  writel((0x1 << 28), 0xc001d020);
-
-  // reset everything
-  //writel(~(1 << 17), 0xc0012004);
-  writel(0, 0xc0012000);
-  writel(0, 0xc0012004);
-  writel(0, 0xc0012008);
-  writel(0xffffffff, 0xc0012004);
-  writel(0xffffffff, 0xc0012000);
-  writel(0xffffffff, 0xc0012008);
-
-
-  // set up 50 Mhz uart clock out of 400 Mhz PLL0 using divider 8
-  writel(   (0 << 15)|        // out clock enable
-            (7 << 5) |        // clock divider
-            (0 << 2) |        // clock source
-            (0 << 1)          // clock invert
-            , 0xC00A9004);
-  writel(1 << 2, 0xC00A9000);         // enable uart clock
-
-  debug_uart_init();
+	board_early_init_f();     // after this, you can call debug()
 
   debug("debug: board_init_f\n");
 
-#if 0
-  //
-  // let's do some verification
-  //
-  {
-    u32   tmp;
-
-    // PLLSETREG0 : 0xc0010008
-    tmp = readl(0xc0010008);
-    if(tmp != 0x101A2602)
-    {
-      hang();
-    }
-
-    // PLLSETREG1
-    tmp = readl(0xc001000c);
-   if(tmp != 0x101A4E04)
-   {
-     hang();
-   }
-
-#if 0
-   // PLLSETREG2
-   tmp = readl(0xc0010010);
-   if(tmp != 0x100CC004)
-   {
-     hang();
-   }
-
-   // PLLSETREG3
-   // XXX
-   // this guy's reset value seems to be different from the one specified in datasheet
-   //
-   tmp = readl(0xc0010014);
-   if(tmp != 0x100C0FA4)
-   {
-     hang();
-   }
-#endif
-
-   //
-   // CLKDIVREG0
-   //
-   tmp = readl(0xc0010020);
-   if(tmp != 0x00008208)
-   {
-     hang();
-   }
-
-#if 1
-   // CLKDIVREG1
-   tmp = readl(0xc0010024);
-   if(tmp != 0x00008208)
-   {
-     hang();
-   }
-#endif
-
-#if 1
-   //
-   // check UART0 enabled
-   //
-   tmp = readl(0xC00A1030);
-   if(!(tmp & (1 << 0 | 1 << 8 | 1 << 9)))
-   {
-     hang();
-   }
-
-   if(tmp & (1 << 1))
-   {
-     hang();
-   }
-#endif
-  }
-#endif
-
 #if 1
   {
-    const int interval = 500000;
+    const u32 interval = 0x00ffffff;
     int cnt = 0;
+    s5p4418_gpio_t*   gpiob = (s5p4418_gpio_t*)S5P4418_BASE_GPIO_B;
 
     while(1)
     {
-
-      for(volatile int i = 0; i < interval; i++) { }
-      writel(0, 0xc001b000);        // set low
-      for(volatile int i = 0; i < interval; i++) { }
-      writel((0x01 << 12), 0xc001b000);        // set high
+      for(u32 i = 0; i < interval; i++) { asm("nop"); }
+      s5p4418_gpio_set_output(gpiob, 12, 0);
+      for(u32 i = 0; i < interval; i++) { asm("nop"); }
+      s5p4418_gpio_set_output(gpiob, 12, 1);
 
       debug("testing ...... %d\n", cnt);
       cnt++;
--- board/samsung/nanopi2-fire/nanopi2_fire.c
+++ board/samsung/nanopi2-fire/nanopi2_fire.c
@@ -3,6 +3,12 @@
 #include <errno.h>
 #include <spl.h>
 #include <serial.h>
+#include <debug_uart.h>
+
+#include <asm/arch/s5p4418.h>
+#include <asm/arch/s5p4418_syscon.h>
+#include <asm/arch/s5p4418_gpio.h>
+#include <asm/arch/s5p4418_clk_gen.h>
 
 int board_init(void)
 {
@@ -37,3 +43,68 @@ struct serial_device *default_serial_console(void)
 }
 #endif
 
+#ifdef CONFIG_DEBUG_UART_BOARD_INIT
+void board_debug_uart_init(void)
+{
+  s5p4418_gpio_t*     gpiod   = (s5p4418_gpio_t*)S5P4418_BASE_GPIO_D;
+  s5p4418_clk_gen_t*  clkgen  = (s5p4418_clk_gen_t*)S5P4418_BASE_UART0_CLKENB;
+
+  //
+  // nanopi2-fire uses UART0 as console
+  //
+
+  //
+  // setup GPIO
+  // UART0 uses GPIOD18/14 for TX/RX.
+  // their alt function is 1
+  //
+  s5p4418_gpio_set_altfn(gpiod, 18, 1);
+  s5p4418_gpio_set_altfn(gpiod, 14, 1);
+
+  //
+  // setup UART clock
+  // only supports 50 Mhz for now
+  // using 1 Ghz PLL0 and clock divider 20
+  //
+  writel( S5P4418_UART_OUTCLKINV0(0)      |
+          S5P4418_UART_CKSRCSEL0(0)       |
+          S5P4418_UART_CLKDIV0(19)        |
+          S5P4418_UART_OUTCLKENB(0),
+          &clkgen->clkgen0l);
+  writel( S5P4418_UART_CLKGEN_ENB(1),
+          &clkgen->clkenb);
+
+}
+#endif
+
+#ifdef CONFIG_BOARD_EARLY_INIT_F
+static inline void
+setup_blue_led(void)
+{
+  //
+  // setup blue LED
+  // GPIO-B12 is connected to LED
+  //
+  s5p4418_gpio_t*   reg = (s5p4418_gpio_t*)S5P4418_BASE_GPIO_B;
+
+  // b12, alt function 2
+  s5p4418_gpio_set_altfn(reg, 12, 2);
+
+  // setup B12 as output
+  s5p4418_gpio_set_io_mode(reg, 12, 1);
+
+  // set B12 high to that LED goes off.
+  s5p4418_gpio_set_output(reg, 12, 0);
+
+}
+
+int board_early_init_f(void)
+{
+  setup_blue_led();
+
+#ifdef CONFIG_DEBUG_UART
+  debug_uart_init();
+#endif
+  return 0;
+}
+#endif
